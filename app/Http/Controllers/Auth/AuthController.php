f6e36c70284161013079b643ddc8ef358906eb5b
seifodev <seifodev@gmail.com>
2018-05-31 16:51:57 +0200

    admin master page setup

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -63,10 +63,21 @@ class AuthController extends Controller
      */
     protected function create(array $data)
     {
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
+
+//        $user = User::create([
+//            'name' => $data['name'],
+//            'email' => $data['email'],
+//            'password' => bcrypt($data['password']),
+//        ]);
+//        // Add default role (subscriber) to the new created user
+//        $role = \App\Role::whereName('subscriber')->first()->users()->save($user);
+//        return $user;
+
     }
 }
